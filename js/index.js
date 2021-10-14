bd191e26f4e302f7acac5fe84a2d41f1a3b7f46d
samtredgett <sam.tredgett@gmail.com>
2021-10-14 15:05:33 +0100

    Added full form validation and error messaging for it.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // I should start coding this 
 
-
-// screen transitions
-
+// ###########################################################################################################
+// SCREEN TRANSITIONS
+// ###########################################################################################################
 // Still has some unknown issue with occassionally returning a 404 error on some screen loads, might be caching 
 window.onload = () => {
     // Select the coloured div for transition effect and anchor elements
@@ -34,10 +34,97 @@ window.onload = () => {
     }
 }
 
-// Form validation
+
+// ###########################################################################################################
+// FORM VALIDATION
+// ###########################################################################################################
+const regForm = document.querySelector('form.contact-form');
+const errors_el = document.querySelector('form.contact-form .errors');
+
+regForm.addEventListener('submit', validateForm);
 
 
+/**
+ * selects form elements as variables
+ * performs regex and empty string checks on them
+ * adds items to error list if they exist
+ * calls errorhandling function handleErrors if list not empty
+ * returns false if errors, true if no errors
+ * @param {EventListener} e 
+ * @returns true/false
+ */
+function validateForm (e) {
+    e.preventDefault();
+
+    const email = document.querySelector('.contact-email');
+    const name = document.querySelector('.contact-name');
+    const message = document.querySelector('.contact-message');
+
+    let errors = [];
+    // Super long regex for email validatoin
+    const email_reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    // Checking for empty fields
+    if(name.value== "") {
+        errors.push({text: "name", el: name});
+    }
+    // email regex or empty validation
+    if(email.value== "" || !email_reg.test(email.value)) {
+        errors.push({text: "email", el: email});
+    } 
+    if(message.value== "") {
+        errors.push({text: "message", el: message});
+    }
+
+    if (errors.length > 0) {
+        alert('Please fill out all fields the form');
+        handleErrors(errors);
+        return false;
+    }
+    alert('Form submitted.')
+    return true;
+
+
+    // Form submission un-needed until backend ready
+    //regForm.submit();
+}
+
+/**
+ * Takes an array of errors with text and a message
+ * pushes error messages in to a string 
+ * focuses page on first error
+ * creates popup error message and inserts into DOM
+ * removes the messages on click.
+ * @param {[array]} errs 
+ * @returns no return, inserts object into the DOM
+ */
+
+function handleErrors (errs) {
+    let str = "You have errors in the following field inputs; ";
+
+    errs.map((er) => {
+        er.el.classList.add('error');
+        
+        str += er.text + ", ";
+    });
+    errs[0].el.focus();
+
+    let error_el = document.createElement('div');
+    error_el.classList.add('error');
+    error_el.innerText = str;
+    error_el.innerHTML += ' <i class="fas errors-close fa-times"></i>';
+    error_el.addEventListener('click', function () {
+        errors_el.removeChild(error_el);
+    })
+    errors_el.appendChild(error_el);
+
+
+}
+// ###########################################################################################################
+// 
+// ###########################################################################################################
 
 // To-Do
+// Tune timings on animations
 // Animate foggy glass rollup for project cards 
 // Create popup box and see if we can make it generic per the type
